f88ae1e0d80b98fc479c789bd4b2203d4eda3980
Toosdai Otte <toosdai52Otte@gmail.com>
2020-02-26 22:15:49 -0600

    finished lecture

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
-
+const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model.js');
 
+//  /api/auth/register
 router.post('/register', (req, res) => {
   let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 10);
+  console.log(hash);
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
@@ -14,13 +19,14 @@ router.post('/register', (req, res) => {
     });
 });
 
+// /api/auth/login
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password,  user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
--- users/users-router.js
+++ users/users-router.js
@@ -1,13 +1,33 @@
 const router = require('express').Router();
-
+const bcrypt = require('bcryptjs');
 const Users = require('./users-model.js');
 
-router.get('/', (req, res) => {
-  Users.find()
+router.get('/', auth, (req, res) => {
+
+
+    Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+  function auth(req, res, next){
+    const {username, password} = req.headers; 
+    
+    Users.findBy({username})
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        console.log("Success!")
+        next();
+      } else {
+        res.status(401).json({ message: "Invalid Credentials"});
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+  }
 
 module.exports = router;
